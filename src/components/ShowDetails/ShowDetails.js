adc8316918ea0f66b9db5e03bf563b1c9621b406
Abdur Nur Tushar Irfan <tusharctg1202@gmail.com>
2022-11-08 04:46:10 +0600

    add review added

--- src/components/ShowDetails/ShowDetails.js
+++ src/components/ShowDetails/ShowDetails.js
@@ -1,36 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../UserContext/UserContext';
 
 
 const ShowDetails = () => {
     const {image_url,details,total_view,title} = useLoaderData()
+    const {user}=useContext(AuthContext)
     
     return (
         <div>
-            <div className="hero min-h-screen bg-base-200">
+            <div className="hero min-h-screen bg-black text-amber-700">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="text-center lg:text-left border">
+                    <div className="text-center lg:text-left border p-6">
                         <p className='text-4xl mb-3 text-cyan-800 text-center'>{title}</p>
                         <img className='h-80 w-full' src={image_url}/>
                         <p className="py-6">{details}</p>
-                        <div>Total-views: {total_view}</div>
+                        <div className='text-center text-2xl'>Total-views: {total_view}</div>
+                        
                     </div>
-                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 ">
                         <div className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
+                                <input type="text" defaultValue={user?.email} placeholder="email" className="input input-bordered" readOnly />
                             </div>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Password</span>
+                                    <span className="label-text">Your Name</span>
                                 </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
+                                <input type="text" placeholder="Your Name" className="input input-bordered" />
+                              
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Add Review</button>
