3872630b191db7d5feb153eacbd4cc7fa5f3c6ce
Abdur Nur Tushar Irfan <tusharctg1202@gmail.com>
2022-11-09 06:24:26 +0600

    form added in add service section

--- src/components/AddService/AddService.js
+++ src/components/AddService/AddService.js
@@ -3,7 +3,16 @@ import React from 'react';
 const AddService = () => {
     return (
         <div>
+            <div className='grid md:grid-cols-1 lg:grid-cols-2 m-10 gap-4'>
+                
+                    <input type="text" placeholder="Type here" className="input input-bordered w-full " />
+                    <input type="text" placeholder="Type here" className="input input-bordered w-full " />
+                    <input type="text" placeholder="Type here" className="input input-bordered w-full " />
+                    <input type="text" placeholder="Type here" className="input input-bordered w-full " />
+                    <button className="btn btn-active btn-primary">Button</button>
             
+            </div>
+
         </div>
     );
 };
